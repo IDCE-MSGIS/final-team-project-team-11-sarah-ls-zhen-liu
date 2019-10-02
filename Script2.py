9d865e2251ea44848da81136d4928fa659ba9375
zhenliu26 <51532833+zhenliu26@users.noreply.github.com>
2019-10-02 15:49:36 -0400

    Program Frame
    1) This is a frame of the whole program.
    2) The "Clothes suggestion" is need to be completed in option 3.
    3)  write-up is need to be added.

--- Script2.py
+++ Script2.py
@@ -1,1 +1,21 @@
 # Place necessary comments and code here. 
+#this is an instruction for users
+tempt=-999999
+while(True):
+    a=input("please select 1): Input the temperature; 2): Convert into centigrade; 3): Cloth suggestion; 4): Exit.\n")
+    if(a==1):
+        tempt=input("please enter the temperature in Fahrenheit:")
+    elif(a==2):
+        if(tempt>-999999):
+            centi=(tempt-32.0)*5/9.0
+            print "Centigrade is",round(centi,3)
+        else:
+            print("Please choose 1 first")
+    elif(a==3):
+        pass
+    elif(a==4):
+        print("thank you for your use!")
+        break
+    else:
+        print("Please input the correct number")
+
