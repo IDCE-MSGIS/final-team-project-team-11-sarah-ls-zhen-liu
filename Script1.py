6832d8844e563fc05e6e31270cc9958c579b5b23
zhenliu26 <51532833+zhenliu26@users.noreply.github.com>
2019-10-02 10:31:53 -0400

    The original script
    This is an original script.:
    1) This update can get the result which required in the moodle.
    2) This script needs to add more write-up and comments.
    3) There should be some more changes to make the whole program more efficient.

--- Script1.py
+++ Script1.py
@@ -1,1 +1,72 @@
-# Place necessary comments and code here. 
+'''
+# Assignment title: Final Project- Web-scraping Weather Forecast
+# Date: 09/23/2019
+# Description: The script web-scrapes the weather.gov website to extract the 5-Day weather forecast for a given location
+# Inputs: Latitude & Longitude in Decimal Degrees
+# Outputs: 5-Day Weather Forecast
+'''
+
+# import required libraries
+import requests
+from bs4 import BeautifulSoup
+
+# List to store response
+forecast = []
+
+## Provide the latitude and longitude for the location you would like to check the forecast for
+## Lat/lon in decimal degrees provided for Worcester, MA
+#get the latitude and longitude from the keyboard
+lat=str(input("Please enter the latitude:"))
+lon=str(input("Please enter the longitude:"))
+
+
+# Create url for the requested location through string concatenation
+url = 'https://forecast.weather.gov/MapClick.php?lat='+lat+"&lon="+lon
+# Check if the URL exists
+# print url
+
+# Send request to retrieve the web-page using the get() function from the requests library
+# The page variable stores the response from the web-page
+page = requests.get(url)
+
+# Create a BeautifulSoup object with the response from the URL
+# Access contents of the web-page using .content
+# html_parser is used since our page is in HTML format
+soup=BeautifulSoup(page.content,"html.parser")
+
+# Locate elements on page to be scraped
+# findAll() locates all occurrences of div tag with the given class name
+# stores it in the BeautifulSoup object
+weather_forecast = soup.findAll("li", {"class": "forecast-tombstone"})
+
+# Loop through the BeautifulSoup object to extract text text from every class instance using .text
+# Store results in a list
+for i in weather_forecast:
+    i = i.text
+    forecast.append(i)
+
+# Print list to remove unicode characters
+for day in forecast:
+    # if(' then' not in day):
+    list_day=day.split('\n')
+    # day.replace('then',' then')
+    if (list_day[3].find(' then')<0):
+      list_day[3]=list_day[3].replace('then',' then')
+    if (list_day[3].find(' and')<0):
+      list_day[3]=list_day[3].replace('and',' and')
+    i=0
+    str1=""
+    # we find that the capital letter which does not preserve its space has the same characteristic. Like "yS" in "MostlySun", the letter in upper case is followed a letter in lower case. So, we go through the whole sentence to find the letter which have this characteristic, and put a space before the letter in upper case.
+    for i in range(len(list_day[3])-1):
+      if(list_day[3][i+1].isupper() and list_day[3][i].islower()):
+        str1=str1+list_day[3][i]+' '
+      else:
+        str1=str1+list_day[3][i]
+    str1=str1+"F"
+    list_day[3]=str1
+    list_day[3]=list_day[3].replace(' Low',', Low')
+    list_day[3]=list_day[3].replace(' High',', High')
+    # Stardard Output
+    print list_day[2].upper()
+    print list_day[3].upper()
+    print("\n")
